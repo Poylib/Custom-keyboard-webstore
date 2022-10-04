c3aa1b0186e0d993fa7e1db79b7948d1b560f2b6
poylib <poy_lib@naver.com>
2022-10-04 17:59:40 +0900

    Fix: 라우트 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import GlobalStyle from './GlobalStyle';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   );
 }
