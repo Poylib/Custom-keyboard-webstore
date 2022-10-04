973a451c9493ac6737b23702c43825f7aa9254f9
poylib <poy_lib@naver.com>
2022-10-04 17:43:57 +0900

    Add: reset style

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  body {
+    background: white;
+    padding-top: 20vw;
+    /* font-family: 'Noto Sans KR', sans-serif; */
+  }
+`;
+
+export default GlobalStyle;
