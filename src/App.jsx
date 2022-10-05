979283f894959dc14be6b52acfc006f2adf93f80
choiseungchul <tmdcjf152@naver.com>
2022-10-05 16:32:58 +0900

    [Add] DetailTop 컴포넌트 반응형 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import GlobalStyle from './GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
-import DetailTop from './pages/detail/DetailTop';
 import Main from './pages/Main';
 import Theme from './pages/Theme';
 import Korean from './pages/keybord/Korean';
 import English from './pages/keybord/English';
+import DetailTop from './pages/detail/DetailTop';
 
 function App() {
   return (
