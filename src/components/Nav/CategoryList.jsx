9d4c33f7f17b26f3869260eeccdbe818b09a1217
poylib <poy_lib@naver.com>
2022-10-24 19:24:12 +0900

    Refactor: 코드 리펙토링

--- src/components/Nav/CategoryList.jsx
+++ src/components/Nav/CategoryList.jsx
@@ -28,25 +28,23 @@ const CategoryList = ({ type, setType }) => {
   }, []);
 
   return (
-    <>
-      <Wrapper>
-        <Swiper slidesPerView={'auto'} style={{}}>
-          {category.map(element => {
-            return (
-              <SwiperSlide
-                key={element}
-                onClick={() => {
-                  setType(element);
-                }}
-                style={{ width: 'auto', paddingBottom: '10px', paddingRight: '20px' }}
-              >
-                <CategoryOne style={type === element ? selectCss : nonSelectCss}>{element}</CategoryOne>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Swiper slidesPerView={'auto'} style={{}}>
+        {category.map(element => {
+          return (
+            <SwiperSlide
+              key={element}
+              onClick={() => {
+                setType(element);
+              }}
+              style={{ width: 'auto', paddingBottom: '10px', paddingRight: '20px' }}
+            >
+              <CategoryOne style={type === element ? selectCss : nonSelectCss}>{element}</CategoryOne>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </Wrapper>
   );
 };
 
--- src/components/themePage/Preview.jsx
+++
@@ -1,4 +0,0 @@
-const Preview = () => {
-  return <></>;
-};
-export default Preview;
